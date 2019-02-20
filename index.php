ebbfa24c2f5a0d8d2cd00a46287d122d56505d4e
nicolamozzato <simonetto.edoardo2003@gmail.com>
2019-02-20 10:25:47 +0100

    esercizio form

--- index.php
+++ index.php
@@ -1,6 +1,44 @@
 <!doctype html>
 <html lang="en">
+  
+   <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
   <body>
+	<title>esercizio form</title>
+	<div class="conteiner">
+	 <form>
+ 		 <div class="form-group">
+   			 <label for="nome">Nome</label>
+    			<input type="text" class="form-control" id="nome" " placeholder="Inserisci il tuo nome">
+  		  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+ 		 </div>
+  		
+  		 <div class="form-group">
+   			 <label for="cognome">Cognome</label>
+    			<input type="text" class="form-control" id="cognome"  placeholder="Inserisci il tuo cognome">
+  		  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+ 		 </div>
+
+ 		 <div class="form-group">
+   			 <label for="indirizzo">Indirizzo</label>
+    			<input type="text" class="form-control" id="indirizzo" rows="5"><textarea>
+  		  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+ 		 </div>
+
+
+
+
+
+
+
+  		<button type="submit" class="btn btn-primary">Submit</button>
+	  </form>
+	</div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
